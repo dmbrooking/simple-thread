a55ee680608cf3429b9798ff1a609a9d317f7471
Dan Brooking <dmbrooking@gmail.com>
2019-02-09 08:09:09 -0500

    Some refactoring. Extracting some logic into methods.

--- lib/project.rb
+++ lib/project.rb
@@ -1,4 +1,8 @@
+require 'forwardable'
+
 class Project < Struct.new(:city, :start_date, :end_date)
+  extend Forwardable
+
   def range
     (start_date..end_date)
   end
@@ -6,4 +10,7 @@ class Project < Struct.new(:city, :start_date, :end_date)
   def length
     range.to_a.length
   end
+
+  def_delegators :city, :travel_day_cost
+  def_delegators :city, :work_day_cost
 end
--- project_reimbursement_calculator.rb
+++ project_reimbursement_calculator.rb
@@ -1,33 +1,54 @@
 class ProjectReimbursementCalculator
-  attr_accessor :projects, :dates_and_rates
+  attr_accessor :projects
+  attr_accessor :dates_and_rates
+  attr_accessor :current_day
 
   def initialize(projects = [])
     @projects = projects
-    total_range = (projects[0].start_date..projects[-1].end_date)
-    @dates_and_rates = Hash[*total_range.collect { |v| [ v, 0 ] }.flatten]
+    @dates_and_rates = Hash[*total_range.collect { |v| [v, 0] }.flatten]
+    @project_for_day = {}
+    @current_day = nil
   end
 
   def run
-    prev_project = nil
+    dates_and_rates.each do |d, _r|
+      @current_day = d
 
-    dates_and_rates.each_with_index do |(day, _rate), index|
-      applied_project = projects.select { |p| p.range.include?(day) }[0]
+      next unless current_project
 
-      unless applied_project
-        dates_and_rates[day-1] = prev_project.city.travel_day_cost
-        prev_project = nil
-        next
-      end
-
-      dates_and_rates[day] =
-        if prev_project.nil? || index == dates_and_rates.size - 1
-          applied_project.city.travel_day_cost
-        else
-          applied_project.city.work_day_cost
-        end
-      prev_project = applied_project
+      dates_and_rates[current_day] = if no_work_yesterday_or_tomorrow?
+                                       current_project.travel_day_cost
+                                     else
+                                       current_project.work_day_cost
+                                     end
     end
 
     dates_and_rates.values.inject { |a, b| a + b }
+  end
+
+  private
+
+  def total_range
+    (projects[0].start_date..projects[-1].end_date)
+  end
+
+  def project_for(day)
+    @project_for_day[day] ||= projects.select { |p| p.range.include?(day) }[0]
+  end
+
+  def prev_project
+    project_for(current_day - 1)
+  end
+
+  def current_project
+    project_for(current_day)
+  end
+
+  def next_project
+    project_for(current_day + 1)
+  end
+
+  def no_work_yesterday_or_tomorrow?
+    prev_project.nil? || next_project.nil?
   end
 end
